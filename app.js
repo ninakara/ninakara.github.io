cdac2bd9f8f6cd04a6844b638d4e245917f6e2ef
ninakara <ninakara12@hotmail.com>
2019-03-13 22:18:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ let counter = 1;
 const size = carouselImages[0].clientWidth;
 
 carouselSlide.style.transform = 'translatex(' + (-size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListeners('click',()=>{
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+carouselSlide.style.transform = 'translatex(' + (-size * counter) + 'px)';
+});
