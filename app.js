2289af721e8b9f7939737343ec4f68921a9a6607
ninakara <ninakara12@hotmail.com>
2019-03-14 01:08:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 //Button Listeners
 
 nextBtn.addEventListeners('click',()=>{
-    if(counter)
+    if (counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListeners('click',()=>{
+    if(counter <=0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
@@ -30,6 +31,13 @@ carouselSlide.addEventListener('transitioned',() => {
     if(carouselImages[counter].id === 'lastClone') {
       carouselSlide.style.transition = "none";
       counter = carouselImages.length - 2;
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+
+    }
+    if(carouselImages[counter].id === 'lastClone') {
+      carouselSlide.style.transition = "none";
+      counter = carouselImages.length - counter;
       carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 
